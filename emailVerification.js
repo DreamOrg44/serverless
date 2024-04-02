0ca54471146e8bc8192fffd662ad91b85f27ce4c
Rushikesh Deore <deore.r@northeastern.edu>
2024-04-02 01:26:43 -0400

    Updating modules

--- emailVerification.js
+++ emailVerification.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const mailgun = require("mailgun-js");
 const { v4: uuidv4 } = require('uuid');
-
+const EmailTracking = require('./model/emailTrackingModel');
 
 const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 async function verifyEmail(userData) {
   try {
@@ -21,11 +21,11 @@ async function verifyEmail(userData) {
     const { id, email } = userData;
 
     const token = uuidv4();
-    
+    const verificationLink = `http://rushikeshdeore.me:3000/verify?token=${token}&userId=${id}`;
+
     await trackEmail(id, email, verificationLink, token);
     console.log(`Email tracking performed for email= ${email}, and token= ${token}`);
 
-    const verificationLink = `http://rushikeshdeore.me/verify?token=${token}&userId=${id}`;
     // const verificationLink = `http://localhost:3000/verify?token=${token}&userId=${id}`;
 
     await sendVerificationEmail(email, verificationLink);
@@ -39,8 +39,8 @@ async function verifyEmail(userData) {
     //   console.error('Failed to send verification email.');
     // }
     // Track the sent email in Cloud SQL
-    
-    
+
+
   } catch (error) {
     console.error('Error verifying the user:', error);
     throw new Error('Error verifying the user:');
@@ -54,7 +54,7 @@ async function sendVerificationEmail(email, verificationLink) {
     from: `WebApp User <no-reply@${process.env.MAILGUN_DOMAIN}>`,
     to: email,
     subject: "Verify Your Email Address for Webapp Access",
-    html: `<p>Click <a href="${verificationLink}&timestamp=${Date.now()}" style="background-color: #4CAF50; /* Green */
+    html: `<p>Click <a href="${verificationLink}" style="background-color: #4CAF50; /* Green */
     border: none;
     color: white;
     padding: 5px 5px;
@@ -64,7 +64,10 @@ async function sendVerificationEmail(email, verificationLink) {
     font-size: 16px;
     margin: 4px 2px;
     cursor: pointer;
-    border-radius: 10px;">Verify Your Email Address</a> to verify your email address.</p>`,
+    border-radius: 10px;">Verify Your Email Address</a> to verify your email address.</p>
+    <p>If you did not sign up for WebApp, please disregard this email.</p>
+    <p>To unsubscribe from future emails, <a href="www.google.com">click here</a>.</p>
+    <p>Best regards,<br/>Dream Org</p>`,
   };
 
   try {
@@ -76,13 +79,29 @@ async function sendVerificationEmail(email, verificationLink) {
   }
 }
 
-async function trackEmail(id, email, verificationLink, verificationToken) {
-  console.log("Inside trackEmail and the values are ", "id= ", id, "email= ", email, "verificationLink= ", verificationLink, "verificationToken= ", verificationToken);
-  const query = {
-    text: 'INSERT INTO email_trackings (id, email, verificationLink,verificationToken) VALUES ($1, $2, $3, $4)',
-    values: [id, email, verificationLink, verificationToken],
-  };
-  await pool.query(query);
+async function trackEmail(userId, email, verificationLink, verificationToken) {
+  console.log("Inside trackEmail and the values are ", "userId= ", userId, "email= ", email, "verificationLink= ", verificationLink, "verificationToken= ", verificationToken);
+  try{
+    await EmailTracking.create({ userId, email, verificationLink, verificationToken });
+    console.log("Email tracking updated successfully");
+  }
+  catch(error){
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      logger.error(error, 'User with this id and token already exists');
+      res.status(400).json({ error: 'User with this id and token already exists' });
+    } else {
+      console.error('Error saving user token information:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+  
+  
+
+  // const query = {
+  //   text: 'INSERT INTO email_trackings (id, email, verificationLink,verificationToken) VALUES ($1, $2, $3, $4)',
+  //   values: [id, email, verificationLink, verificationToken],
+  // };
+  // await pool.query(query);
 }
 
 module.exports = {
